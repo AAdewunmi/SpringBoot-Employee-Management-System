d9075ccaa207a52fe3c32f7e30ccd7848323d214
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-30 14:27:54 +0000

    Initial Project Commit

---
+++ src/test/java/com/thymeleafspringbootapplication/employeemanagementsystem/SpringBootEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafspringbootapplication.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
