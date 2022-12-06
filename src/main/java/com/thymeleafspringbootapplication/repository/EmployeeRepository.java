4e5072268757d8a4921d34b08cc7545529c5014c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-06 12:35:59 +0000

    Sixth Commit: Extend EmployeeRepository by using JpaRepository API for CRUD operations.

--- src/main/java/com/thymeleafspringbootapplication/repository/EmployeeRepository.java
+++ src/main/java/com/thymeleafspringbootapplication/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.thymeleafspringbootapplication.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thymeleafspringbootapplication.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
