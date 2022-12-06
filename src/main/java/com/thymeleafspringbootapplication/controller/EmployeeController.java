5e59e4107d465789496c6c69dd293cced71e884d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-06 15:53:43 +0000

    Twelve Commit: Implement viewHomePage() method in EmployeeController class

--- src/main/java/com/thymeleafspringbootapplication/controller/EmployeeController.java
+++ src/main/java/com/thymeleafspringbootapplication/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package com.thymeleafspringbootapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.thymeleafspringbootapplication.service.EmployeeService;
+
+@Controller
 public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	// Display list of Employees
+	
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		return "index";
+	}
 
 }
