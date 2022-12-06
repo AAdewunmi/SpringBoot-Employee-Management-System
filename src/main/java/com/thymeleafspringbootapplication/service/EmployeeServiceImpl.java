cf2ac8b6599c5a3435fdd5fe403a4fada651ada0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-06 15:07:34 +0000

    Ten Commit: Implement getAllEmployees() method in EmployeeServiceImpl class

--- src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
+++ src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
@@ -1,5 +1,20 @@
 package com.thymeleafspringbootapplication.service;
 
-public class EmployeeServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.thymeleafspringbootapplication.model.Employee;
+import com.thymeleafspringbootapplication.repository.EmployeeRepository;
+
+public class EmployeeServiceImpl implements EmployeeService{
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
 
 }
