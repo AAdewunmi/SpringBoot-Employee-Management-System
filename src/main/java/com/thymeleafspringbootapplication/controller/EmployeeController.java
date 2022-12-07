e16cd0397003007f4267d6c053b6cb56dabb973d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-07 16:54:57 +0000

    Twenty Commit: Implement form handler method showNewEmployeeForm() in EmployeeController class

--- src/main/java/com/thymeleafspringbootapplication/controller/EmployeeController.java
+++ src/main/java/com/thymeleafspringbootapplication/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.thymeleafspringbootapplication.model.Employee;
 import com.thymeleafspringbootapplication.service.EmployeeService;
 
 @Controller
@@ -14,11 +15,20 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 	
 	// Display list of Employees
-	
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
 		model.addAttribute("listEmployees", employeeService.getAllEmployees());
 		return "index";
+	}
+	
+	
+	// Add new Employee
+	@GetMapping("/showNewEmployeeForm")
+	public String showNewEmployeeForm(Model model) {
+		// Create model attribute to bind form data
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "new_employee";
 	}
 
 }
