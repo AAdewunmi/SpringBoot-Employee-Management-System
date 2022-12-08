47681dbe88de7d51d095609e2da3d088449b450d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-08 10:26:07 +0000

    Twenty-Three Commit: Implement form handler method saveEmployee() in EmployeeController class

--- src/main/java/com/thymeleafspringbootapplication/controller/EmployeeController.java
+++ src/main/java/com/thymeleafspringbootapplication/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.thymeleafspringbootapplication.model.Employee;
 import com.thymeleafspringbootapplication.service.EmployeeService;
@@ -29,6 +31,12 @@ public class EmployeeController {
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "new_employee";
+	}
+	
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.saveEmployee(employee);
+		return "redirect:/";
 	}
 
 }
