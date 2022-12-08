7c7b971fcea21f788b510135277fd612f7742632
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-08 11:51:02 +0000

    Twenty-Five Commit: Implement getEmployeeById() method in EmployeeServiceImpl Class

--- src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
+++ src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
@@ -1,6 +1,9 @@
 package com.thymeleafspringbootapplication.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,18 @@ public class EmployeeServiceImpl implements EmployeeService{
 	@Override
 	public void saveEmployee(Employee employee) {
 		this.employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee;
+		if (optional.isPresent()) {
+			employee =  optional.get();
+		} else {
+			throw new RuntimeException(" Employee not found for id :: " + id);
+		}
+		return employee;
 	}
 
 }
