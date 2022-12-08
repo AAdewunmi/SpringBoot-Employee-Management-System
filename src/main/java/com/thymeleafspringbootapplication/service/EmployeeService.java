23cef1da7c0ffd18af84e3e2e255f99eca6c2e51
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-08 10:39:53 +0000

    Twenty-Four Commit: Declare getEmployeeById() method in EmployeeService interface.

--- src/main/java/com/thymeleafspringbootapplication/service/EmployeeService.java
+++ src/main/java/com/thymeleafspringbootapplication/service/EmployeeService.java
@@ -7,4 +7,5 @@ import com.thymeleafspringbootapplication.model.Employee;
 public interface EmployeeService {
 	List<Employee> getAllEmployees();
 	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
 }
