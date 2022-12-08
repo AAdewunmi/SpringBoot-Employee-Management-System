d9fcde214ac6d68661531b288793356c057f69d2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-08 13:51:06 +0000

    Thirty-Two Commit:  Implement deleteEmployeeById() method in EmployeeServiceImpl Class

--- src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
+++ src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
@@ -37,4 +37,9 @@ public class EmployeeServiceImpl implements EmployeeService{
 		return employee;
 	}
 
+	@Override
+	public void deleteEmployeeById(long id) {
+		this.employeeRepository.deleteById(id);
+	}
+
 }
